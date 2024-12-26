ff8b299c149c2bb2bf4911c82b60481825e962f2
leofacts26 <leofacts26@gmail.com>
2024-12-26 10:25:27 +0530

    bug fix

--- src/pages/Subscription.jsx
+++ src/pages/Subscription.jsx
@@ -62,7 +62,7 @@ const Subscription = () => {
 
                   <Stack direction="row" justifyContent="space-between" alignItems="center" className="mt-3">
                     <p className="subscription-type">Subscription Plan:</p>
-                    <Button variant="contained" className="subscribe-btn">
+                    <Button variant="contained" className="subscribe-btn" style={{ backgroundColor: activeSubscriptionList?.activeSubscription?.display_color || '#57636c' }}>
                       {activeSubscriptionList?.activeSubscription?.subscription_name ? activeSubscriptionList?.activeSubscription?.subscription_name : 'N/A'} </Button>
                   </Stack>
 
