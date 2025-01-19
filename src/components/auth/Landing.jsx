be82da2101da17dd96b744a0a51d1a43eb974147
leofacts26 <leofacts26@gmail.com>
2025-01-19 20:23:27 +0530

    landing fix

--- src/components/auth/Landing.jsx
+++ src/components/auth/Landing.jsx
@@ -7,19 +7,19 @@ const Landing = () => {
     return (
         <>
             <section className='landing-bg'>
-                <Stack  direction="column">
-                <h1 className='pb-5 ct-box-landing-title'>Welcome to Caterings & Tiffins</h1>
-                <div className="ct-landing-box">
-                    <h2 className='ct-box-title'>Register Your Business</h2>
-                    <div className='mb-3' style={{ marginTop: '20px', borderTop: '1px solid #e0e3e7' }}>
-                        <Divider />
+                <Stack direction="column">
+                    <h1 className='pb-5 ct-box-landing-title'>Welcome to Caterings & Tiffins</h1>
+                    <div className="ct-landing-box">
+                        <h2 className='ct-box-title'>Register Your Business</h2>
+                        <div className='mb-3' style={{ marginTop: '20px', borderTop: '1px solid #e0e3e7' }}>
+                            <Divider />
+                        </div>
+                        <p className='text-center landing-para'>Please choose your service below</p>
+                        <Stack direction="column" justifyContent="center" className='text-center'>
+                            <Link to="https://cateringvendor.cateringsandtiffins.com/create-account" className='mb-4 mt-5'> <Button variant="contained" className='ct-box-btn-catering'>Catering Service</Button> </Link>
+                            <Link to="https://tiffinvendor.cateringsandtiffins.com/create-account" className=''>  <Button variant="contained" className='ct-box-btn-tiffin'>Tiffin Service</Button> </Link>                   
+                             </Stack>
                     </div>
-                    <p className='text-center landing-para'>Please choose your service below</p>
-                    <Stack direction="column" justifyContent="center" className='text-center'>
-                        <Link to="/create-account" className='mb-4 mt-5'> <Button variant="contained" className='ct-box-btn-catering'>Catering Service</Button> </Link>
-                        <Button variant="contained" className='ct-box-btn-tiffin' style={{color: '#ffffff'}}>Tiffin Service</Button>
-                    </Stack>
-                </div>
                 </Stack>
             </section>
         </>
