7899f9c3fdf0a2008f96fa7837e506cb51a976c8
leofacts26 <leofacts26@gmail.com>
2025-02-09 21:50:01 +0530

    bug fix

--- src/components/global/InquiryCard.jsx
+++ src/components/global/InquiryCard.jsx
@@ -2,8 +2,10 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import PhoneIcon from '@mui/icons-material/Phone';
 import toast from 'react-hot-toast';
+import moment from "moment";
 
-const InquiryCard = ({item}) => {
+
+const InquiryCard = ({ item }) => {
 
     const handleCopyText = (number) => {
         navigator.clipboard.writeText(number)
@@ -20,10 +22,16 @@ const InquiryCard = ({item}) => {
         <>
             <Stack className="inquiries-box mb-3" direction="row" justifyContent="space-between" alignItems="center">
                 <div>
-                    <h4 className="inquiries-title">{item?.user_name}</h4>
-                    <p className="inquiries-date">{item?.enquiry_date.slice(0,10)}</p>
-                    <p className="inquiries-desc">{item?.user_phone_number}</p>
-                    {/* <p className="inquiries-desc mt-1">{item?.description}</p> */}
+                    <div>
+                        <h4 className="inquiries-title">{item?.user_name}</h4>
+                        <p className="inquiries-date">{item?.area} </p>
+                        <p className="inquiries-date"> {item.food_types.length > 0 && item.food_types?.map((item) => item?.food_type_name)} </p>
+                        <p className="inquiries-desc mb-1">{item?.cuisines.length > 0 && item?.cuisines.map((item) => <span>{item.cuisine_name}, {" "}</span>)}</p>
+                        <p className="inquiries-desc mb-1">{item?.service_types.length > 0 && item?.service_types.map((item) => <span>{item.service_type_name}, {" "}</span>)}</p>
+                        <p className="inquiries-desc mb-1">{item?.occasions.length > 0 && item?.occasions.map((item) => <span>{item.occasion_name}, {" "}</span>)}</p>
+                        <p className="inquiries-date">  {moment(item?.enquiry_date).format("DD MMM, YYYY")}</p>
+                        <p className="inquiries-desc">{item?.user_phone_number}</p>
+                    </div>
                 </div>
                 <div>
                     <Button variant="contained" className="inquiries-btn" onClick={() => handleCopyText(item?.user_phone_number)}> <PhoneIcon style={{ fontSize: '14px' }} className="me-2" /> Call now</Button>
